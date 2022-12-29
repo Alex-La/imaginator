013e6f05a1030e02b83c5f758c098a1ee8ee65c2
Alex Larionov <lars6162@gmail.com>
2022-12-30 02:39:57 +0300

    Update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,11 @@
 import {defineConfig, loadEnv} from "vite"
-import _ from "lodash"
 import react from "@vitejs/plugin-react"
 import TsconfigPaths from "vite-tsconfig-paths"
 import WindiCSS from "vite-plugin-windicss"
 import PluginRewriteAll from "vite-plugin-rewrite-all"
 
 export default defineConfig(({mode}) => {
-  if (mode === "development") {
-    const env = loadEnv(mode, process.cwd(), "")
-    const isEnvEmpty = _.isEmpty(env)
-    if (isEnvEmpty) throw new Error(`Please, provide ${mode} env config.`)
-  }
+  process.env = {...process.env, ...loadEnv(mode, process.cwd(), "")}
 
   return {
     server: {
